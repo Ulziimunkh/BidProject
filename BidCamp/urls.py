33f532dd979e13dc2f4f03009e8c6599eda51e22
Ulziimunkh <uujgii36@gmail.com>
2018-10-16 00:29:49 +0530

    changes project structure with template

--- BidCamp/urls.py
+++ BidCamp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
+app_name= 'BidCamp'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('^/single', views.single, name='single'),
+    path('single/', views.single, name='single'),
 ]
